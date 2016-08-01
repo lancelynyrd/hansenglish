4988173a7cc5e30d05833968ef8b6bb3caa15b80
lancelynyrd <you@example.com>
2016-08-01 23:31:30 +0800

    additional changes on hans

--- m-ch-class-detail-1.php
+++ m-ch-class-detail-1.php
@@ -52,7 +52,7 @@
         <img src="<?php echo td() ?>/m_img/mobile_logo_1.jpg" width="100%">
     </div>
     <div id='con_02'>
-        <a href="m-ch-index"><img src="<?php echo td() ?>/m_img/m_bt_home_1.jpg" border="0"></a><a href="m-ch-how-study"><img src="<?php echo td() ?>/m_img/m_bt_intro_study.jpg"  border="0"></a><a href="m-ch-class-detail-1"><img src="<?php echo td() ?>/m_img/m_bt_how_study.jpg"  border="0"></a><a href="m-ch-use-skype"><img src="<?php echo td() ?>/m_img/m_bt_skype.jpg"  border="0"></a><a href="m-ch-free-class"><img src="<?php echo td() ?>/m_img/m_bt_free_class.jpg"  border="0"></a><a href="m-ch-fee"><img src="<?php echo td() ?>/m_img/m_bt_fee.jpg"  border="0"></a>
+        <a href="m-ch-index"><img src="<?php echo td() ?>/m_img/m_bt_home.jpg" border="0"></a><a href="m-ch-how-study"><img src="<?php echo td() ?>/m_img/m_bt_intro_study.jpg"  border="0"></a><a href="m-ch-class-detail-1"><img src="<?php echo td() ?>/m_img/m_bt_how_study_1.jpg"  border="0"></a><a href="m-ch-use-skype"><img src="<?php echo td() ?>/m_img/m_bt_skype.jpg"  border="0"></a><a href="m-ch-free-class"><img src="<?php echo td() ?>/m_img/m_bt_free_class.jpg"  border="0"></a><a href="m-ch-fee"><img src="<?php echo td() ?>/m_img/m_bt_fee.jpg"  border="0"></a>
     </div>
 </div>
 
--- m-ch-class-detail-6.php
+++ m-ch-class-detail-6.php
@@ -44,7 +44,7 @@
         <img src="<?php echo td() ?>/m_img/mobile_logo_1.jpg" width="100%">
     </div>
     <div id='con_02'>
-        <a href="m-ch-index"><img src="<?php echo td() ?>/m_img/m_bt_home_1.jpg" border="0"></a><a href="m-ch-how-study"><img src="<?php echo td() ?>/m_img/m_bt_intro_study.jpg"  border="0"></a><a href="m-ch-class-detail-1"><img src="<?php echo td() ?>/m_img/m_bt_how_study.jpg"  border="0"></a><a href="m-ch-use-skype"><img src="<?php echo td() ?>/m_img/m_bt_skype.jpg"  border="0"></a><a href="m-ch-free-class"><img src="<?php echo td() ?>/m_img/m_bt_free_class.jpg"  border="0"></a><a href="m-ch-fee"><img src="<?php echo td() ?>/m_img/m_bt_fee.jpg"  border="0"></a>
+        <a href="m-ch-index"><img src="<?php echo td() ?>/m_img/m_bt_home.jpg" border="0"></a><a href="m-ch-how-study"><img src="<?php echo td() ?>/m_img/m_bt_intro_study.jpg"  border="0"></a><a href="m-ch-class-detail-1"><img src="<?php echo td() ?>/m_img/m_bt_how_study.jpg"  border="0"></a><a href="m-ch-use-skype"><img src="<?php echo td() ?>/m_img/m_bt_skype.jpg"  border="0"></a><a href="m-ch-free-class"><img src="<?php echo td() ?>/m_img/m_bt_free_class.jpg"  border="0"></a><a href="m-ch-fee"><img src="<?php echo td() ?>/m_img/m_bt_fee_1.jpg"  border="0"></a>
     </div>
 </div>
 
@@ -55,7 +55,7 @@
 
 <div id='03'>
     <div class='ss' align="center" style=' height: 100px;  background:url(<?php echo td() ?>/img/class/back.jpg);'>
-        <a href="m-ch-class-detail-1"><img src="<?php echo td() ?>/img/class/m_10.jpg" border="0"></a><a href="m-ch-class-detail-2"><img src="<?php echo td() ?>/img/class/m_11.jpg" border="0" ></a><a href="m-ch-class-detail-3"><img src="<?php echo td() ?>/img/class/12.jpg" border="0"></a><a href="m-ch-class-detail-4"><img src="<?php echo td() ?>/img/class/13.jpg"  border="0" ></a><a href="m-ch-class-detail-5"><img src="<?php echo td() ?>/img/class/14.jpg" border="0"></a><a href="m-ch-class-detail-6"><img src="<?php echo td() ?>/img/class/15_1.jpg" border="0" onmouseout="this.src='<?php echo td() ?>/img/class/15.jpg'"></a>
+        <a href="m-ch-class-detail-1"><img src="<?php echo td() ?>/img/class/m_10.jpg" border="0"></a><a href="m-ch-class-detail-2"><img src="<?php echo td() ?>/img/class/m_11.jpg" border="0" ></a><a href="m-ch-class-detail-3"><img src="<?php echo td() ?>/img/class/12.jpg" border="0"></a><a href="m-ch-class-detail-4"><img src="<?php echo td() ?>/img/class/13.jpg"  border="0" ></a><a href="m-ch-class-detail-5"><img src="<?php echo td() ?>/img/class/14.jpg" border="0"></a><a href="m-ch-class-detail-6"><img src="<?php echo td() ?>/img/class/15_1.jpg" border="0" onmouseout="this.src='<?php echo td() ?>/img/class/15_1.jpg'"></a>
     </div>
 </div>
 
