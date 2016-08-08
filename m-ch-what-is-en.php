ee48ae366fd2f98f971c011772174c943630708e
lancelynyrd <you@example.com>
2016-08-08 18:09:23 +0800

    updating video

--- m-ch-what-is-en.php
+++ m-ch-what-is-en.php
@@ -1,26 +1,26 @@
 <style>
-    .video_clip embed {
+    .video_clip {
         width: 100%;
         height: 200px;
     }
 
     @media all and (min-width: 380px) {
-        .video_clip embed {
+        .video_clip {
             height: 275px;
         }
     }
     @media all and (min-width: 600px) {
-        .video_clip embed {
+        .video_clip {
             height: 300px;
         }
     }
     @media all and (min-width: 800px) {
-        .video_clip embed {
+        .video_clip {
             height: 450px;
         }
     }
     @media all and (min-width: 1024px) {
-        .video_clip embed {
+        .video_clip {
             width: 960px;
             height: 550px;
         }
@@ -28,20 +28,11 @@
 </style>
 
 <div id='content'>
-    <div id='con_03'>
+    <div id='con_03' style="text-align: center;">
         <br><br>
-        <center>
-
-            <object class="video_clip">
-                <param name="Filename" value="img/mv/woo.mp4">
-                <param name="AutoStart" value="true">
-                <param name="ShowControls" value="true">
-                <embed src="<?php echo td() ?>/img/mv/woo.mp4" showcontrols="true" autostart>
-                </embed>
-            </object>
-
-        </center>
-
+        <video controls="" autoplay="" name="media" class="video_clip">
+            <source src="<?php echo td() ?>/img/mv/woo.mp4" type="video/mp4">
+        </video>
         <br><br>
     </div>
 </div>
