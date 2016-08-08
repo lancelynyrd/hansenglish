1c2741ba3c5087d8362044547d85b4836feed7ce
lancelynyrd <you@example.com>
2016-08-08 15:26:46 +0800

    update the top menu button

--- m-ch-hongbao.php
+++ m-ch-hongbao.php
@@ -1,3 +1,17 @@
+<style>
+    ul.text_info {
+        font-size: 18px;
+        margin-left: 10px;
+    }
+    @media all and ( min-width: 480px ) {
+        ul.text_info {
+            font-size: 24px;
+            margin-left: 20px;
+        }
+    }
+</style>
+
+
 <div id='03'>
     <img src="<?php echo td() ?>/m_img/m_fee.jpg" width="100%">
     <br><br><br>
@@ -5,32 +19,28 @@
 
 <!-- 본문 시작 -->
 
-<br><br>
-
 <img src="<?php echo td() ?>/m_img/hongbao-1.jpg" border="0" width="100%">
 
-<br>
-
-<div style="width: 100%; height: 446px;">
-    <div style="width: 50%; height: 446px; float: left;">
-        <img src="<?php echo td() ?>/m_img/mul_1.jpg" border="0" >
-    </div>
-    <div style="width: 50%; height: 446px; float: left; margin-left: 0px;">
+<div style="width: 100%;">
+    <div>
         <br><br><br>
-        <ul>
+        <ul class="text_info">
             <li>Hans English 不打广告</li>
             <li>合理的学费</li>
             <li>和会员分享利润</li>
             <li>会员介绍会员</li>
         </ul>
     </div>
+    <div>
+        <img src="<?php echo td() ?>/m_img/mul_1.jpg" border="0" width="100%" >
+    </div>
 </div>
 
 <br><br><br>
 
 
-<div id='04' style="background-color: #fa2a91; padding: 40px;">
-    <ul>
+<div id='04' style="background-color: #fa2a91; padding: 20px 10px;">
+    <ul class="text_info">
         <li style="color: white;">交费800元课程专用红包</li>
         <li style="color: white;">成功介绍会员学习一个月，每个人500元介绍费</li>
         <li style="color: white;">介绍会员学习第二个月开始下图模式</li>
--- m-ch-what-is-en.php
+++ m-ch-what-is-en.php
@@ -1,14 +1,43 @@
+<style>
+    .video_clip embed {
+        width: 100%;
+        height: 200px;
+    }
+
+    @media all and (min-width: 380px) {
+        .video_clip embed {
+            height: 275px;
+        }
+    }
+    @media all and (min-width: 600px) {
+        .video_clip embed {
+            height: 300px;
+        }
+    }
+    @media all and (min-width: 800px) {
+        .video_clip embed {
+            height: 450px;
+        }
+    }
+    @media all and (min-width: 1024px) {
+        .video_clip embed {
+            width: 960px;
+            height: 550px;
+        }
+    }
+</style>
+
 <div id='content'>
     <div id='con_03'>
-        <br><br><br><br><br>
+        <br><br>
         <center>
 
-            <object width="1280" height="720">
+            <object class="video_clip">
                 <param name="Filename" value="img/mv/woo.mp4">
                 <param name="Autosize" value="true">
                 <param name="AutoStart" value="true">
                 <param name="ShowControls" value="true">
-                <embed src="<?php echo td() ?>/img/mv/woo.mp4" width="1280" height="720" showcontrols="true" autostart>
+                <embed src="<?php echo td() ?>/img/mv/woo.mp4" showcontrols="true" autostart>
                 </embed>
             </object>
 
