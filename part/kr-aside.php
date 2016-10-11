f548cf7209930540f723b874377101b6466aef32
lancelynyrd <pinedaclp@gmail.com>
2016-10-11 16:40:55 +0800

    updating the images and text

--- part/kr-aside.php
+++ part/kr-aside.php
@@ -1,7 +1,7 @@
 <aside class="sidebar">
     <ul>
-        <li><a href="<?php vc_url()?>" target="_blank"><img src="<?php img_e() ?>aside/enter_class.jpg"><span><?php _text("") ?></span></a></li>
-        <li><a href="http://rj.baidu.com/soft/detail/14744.html?ald"><img src="<?php img_e() ?>aside/chrome.jpg" ><span><?php _text("") ?></span></a></li>
+        <li><a href="<?php vc_url()?>" target="_blank"><img src="<?php img_e() ?>aside/kr-enter_class.jpg"><span><?php _text("") ?></span></a></li>
+        <li><a href="https://www.google.com/chrome/browser/desktop/index.html"><img src="<?php img_e() ?>aside/kr-chrome.jpg" ><span><?php _text("") ?></span></a></li>
     </ul>
 </aside>
 
