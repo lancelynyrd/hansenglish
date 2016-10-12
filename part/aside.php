2729fc829e376c1e0ec94cd6a1d485692fc020b1
lancelynyrd <pinedaclp@gmail.com>
2016-10-12 17:40:06 +0800

    adding the full installer of chrome

--- part/aside.php
+++ part/aside.php
@@ -1,7 +1,7 @@
 <aside class="sidebar">
     <ul>
         <li><a href="<?php vc_url()?>" target="_blank"><img src="<?php img_e() ?>aside/enter_class.jpg"><span><?php _text("") ?></span></a></li>
-        <li><a href="http://rj.baidu.com/soft/detail/14744.html?ald" target="_blank"><img src="<?php img_e() ?>aside/chrome.jpg" ><span><?php _text("") ?></span></a></li>
+        <li><a href="<?php echo td(); ?>/download/ChromeStandaloneSetupCN.exe" target="_blank"><img src="<?php img_e() ?>aside/chrome.jpg" ><span><?php _text("") ?></span></a></li>
     </ul>
 </aside>
 
