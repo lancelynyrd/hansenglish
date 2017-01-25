0574b0f64255fd9871c18112f77ed820da93126f
lancelynyrd <pinedaclp@gmail.com>
2017-01-25 18:54:33 +0800

    changing chrome download link

--- part/kr-aside.php
+++ part/kr-aside.php
@@ -1,7 +1,7 @@
 <aside class="sidebar">
     <ul>
         <li><a href="<?php vc_url()?>" target="_blank"><img src="<?php img_e() ?>aside/kr-enter_class.png"><span><?php _text("") ?></span></a></li>
-        <li><a href="<?php echo td(); ?>/download/ChromeStandaloneSetupKR.exe" target="_blank"><img src="<?php img_e() ?>aside/kr-chrome.png" ><span><?php _text("") ?></span></a></li>
+        <li><a href="http://hansnet.applinzi.com/down/chrome_int.zip" target="_blank"><img src="<?php img_e() ?>aside/kr-chrome.png" ><span><?php _text("") ?></span></a></li>
     </ul>
 </aside>
 
